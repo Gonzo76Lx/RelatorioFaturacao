a6701061b39f8a28654f103e8cfb484b4f0045fa
Gonçalo Nuno Samudio Ferreira dos Santos <gnfsantos@gmail.com>
2026-08-20 12:19:31 +0100

    Add InverseBoolConverter and LogAssinaturaFicheiro model class

---
+++ RelatorioFaturacao/Models/LogAssinaturaFicheiro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RelatorioFaturacao.Models
+{
+    public class LogAssinaturaFicheiro
+    {
+        public string? NomeFicheiro { get; set; }
+        public DateTime DataProcessamento { get; set; }
+        public string? Estado { get; set; }
+        public int numRetry { get; set; }
+        public string? MensagemErro { get; set; }
+    }
+}
